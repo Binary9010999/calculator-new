03e3a301f5faae0de7b559153da1ff79a287af52
Binary9010999 <149241923+Binary9010999@users.noreply.github.com>
2023-10-28 19:12:32 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        type: "number",
+        name: "numberOne",
+        message: "Kindly enter your first number: "
+    },
+    {
+        type: "number",
+        name: "numberTwo",
+        message: "Kindly enter your Second number: "
+    },
+    {
+        type: "list",
+        name: "Operator",
+        choices: ["*", "+", "-", "/",],
+        message: "Select Operator: "
+    },
+]);
+const { numberOne, numberTwo, Operator } = answer;
+if (numberOne && numberTwo && Operator) {
+    let result = 0;
+    if (Operator === "+") {
+        result = numberOne + numberTwo;
+    }
+    else if (Operator === "-") {
+        result = numberOne - numberTwo;
+    }
+    if (Operator === "/") {
+        result = numberOne / numberTwo;
+    }
+    if (Operator === "*") {
+        result = numberOne * numberTwo;
+    }
+    console.log("Your result is :", result);
+}
+else {
+    console.log("Kindly enter valid input");
+}
